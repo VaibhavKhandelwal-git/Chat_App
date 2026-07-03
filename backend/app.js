3b309300883860eff65fc41aca4b5cad45184e2e
Vaibhav-Khandelwal007 <vaibhavkhandewal23cs@rnsit.ac.in>
2026-07-04 01:42:41 +0530

    aready for Deployment

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./src/routes/auth.route.js";
 import messageRoutes from "./src/routes/message.route.js";
@@ -8,6 +9,7 @@ import messageRoutes from "./src/routes/message.route.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,5 +18,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT || 3000;
+
+
+//Make ready for Deployment
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend", "dist")));
+
+    app.get("*", (_req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
